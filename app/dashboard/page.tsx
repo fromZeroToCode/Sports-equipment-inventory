e26e934d3ef3dfc123d195ae00809d1d8e40db94
Swif7ify <earl.coding@gmail.com>
2025-09-25 00:50:20 +0800

    removed suspense in thge dashboard/page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,7 @@
-// export const revalidate = 0;
-
-import React, { Suspense } from "react";
-// import DashboardClient from "@/components/dashboard/DashboardClient";
+"use client";
+import React from "react";
+import DashboardClient from "@/components/dashboard/DashboardClient";
 
 export default function DashboardPage() {
-	return (
-		<div className="p-8">Dashboard is under construction...</div>
-		// <Suspense fallback={<div className="p-8">Loading dashboard...</div>}>
-		// 	<DashboardClient />
-		// </Suspense>
-	);
+	return <DashboardClient />;
 }
